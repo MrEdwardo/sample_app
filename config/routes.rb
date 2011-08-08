6055a67da213fc50a308489748893775a9744edb
Ed Shelley <edshelley@gmail.com>
2011-08-08 21:52:13 +0100

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 SampleApp::Application.routes.draw do
 
-  resources :users
+  get "sessions/new"
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signout',  :to => 'sessions#destroy'
+  match '/signin',  :to => 'sessions#new'
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -45,6 +45,21 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end
+
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[email='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      response.should have_selector("input[password='user[password]'][type='text']")
+    end
+
   end
   
   
@@ -80,21 +95,26 @@ describe UsersController do
                       :password => "foobar", :password_confirmation => "foobar" }
       end
 
-          it "should create a user" do
-            lambda do
-              post :create, :user => @attr
-            end.should change(User, :count).by(1)
-          end
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
 
-          it "should redirect to the user show page" do
-            post :create, :user => @attr
-            response.should redirect_to(user_path(assigns(:user)))
-          end
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
           
-          it "should have a welcome message" do
-            post :create, :user => @attr
-            flash[:success].should =~ /welcome to the sample app/i
-          end
-        end
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+    end         
   end
 end
